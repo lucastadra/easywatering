939bc5fc09abbdcd555dda9ad049a16f0b7a4511
Lucas Tadra Mainginski <lucastadra16@gmail.com>
2022-08-14 19:57:13 -0300

    remoção de arquivos inutilizados

--- Server/tests/WS/mqtt.js
+++
@@ -1,6 +0,0 @@
-const mqtt = require('mqtt') 
-require('dotenv').config() 
-
-
-const clientId = 'mqttjs_' + Math.random().toString(8).substr(2, 4) 
-const client  = mqtt.connect(process.env.BROKER_URL, {clientId: clientId}); 
